77ebf81c95ce8a1863ac58a838cd7c37a630d591
devansh071999 <devanshsampatjain@gmail.com>
2020-02-15 13:41:04 +0530

    Fragment added

---
+++ app/src/main/java/com/example/smart_education_hack36/fragments/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.smart_education_hack36.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smart_education_hack36.R;
+
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
