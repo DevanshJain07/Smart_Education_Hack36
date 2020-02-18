21c158091f2446a9157e49d805f1949aacb7f3ed
BhuwanChandra <bhuwan7779@gmail.com>
2020-02-19 00:45:05 +0530

    updated

--- Hack36/flask/static/js/graph.js
+++ Hack36/flask/static/js/graph.js
@@ -9,6 +9,63 @@ function createGraph(dataFile, chart, equation){
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var x = d3.scalePolynomial()
+        .range([0, width]);
+
+    var y = d3.scalePolynomial()
+        .range([height, 0]);
+
+    var xAxis = d3.axisBottom()
+        .scale(x);
+
+    var yAxis = d3.axisLeft()
+        .scale(y);
+
+    d3.csv(dataFile, types, function (error, data) {
+
+        y.domain(d3.extent(data, function (d) { return d.y }));
+        x.domain(d3.extent(data, function (d) { return d.x }));
+
+        // see below for an explanation of the calcLinear function
+        var lg = calcLinear(data, "x", "y", d3.min(data, function (d) { return d.x }), d3.min(data, function (d) { return d.x }), equation);
+
+        svg.append("line")
+            .attr("class", "regression")
+            .attr("x1", x(lg.ptA.x))
+            .attr("y1", y(lg.ptA.y))
+            .attr("x2", x(lg.ptB.x))
+            .attr("y2", y(lg.ptB.y));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        svg.selectAll(".point")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "point")
+            .attr("r", 3)
+            .attr("cy", function (d) { return y(d.y); })
+            .attr("cx", function (d) { return x(d.x); });
+
+    });
+}
+function createGraph(dataFile, chart, equation){
+        var margin = { top: 5, right: 5, bottom: 15, left: 15 },
+        width = 340 - margin.left - margin.right,
+        height = 340 - margin.top - margin.bottom;
+
+    var svg = d3.select(chart).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     var x = d3.scaleLinear()
         .range([0, width]);
 
@@ -55,6 +112,7 @@ function createGraph(dataFile, chart, equation){
 
     });
 }
+
 
 function types(d) {
     d.x = +d.x;
